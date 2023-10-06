90469d76d889de3316949734b2b1cb114dec0404
Niklesh <tiwaneniklesh9@gmail.com>
2023-10-06 14:09:16 +0000

    minor update

--- hoverableSidebarMenu/script.js
+++ hoverableSidebarMenu/script.js
@@ -1,15 +1,21 @@
 const sidebar = document.querySelector('.sidebar')
 const lockIcon = document.querySelector('#lock-icon')
 const sidebarClose = document.querySelector('#sidebar-close')
+const menuContainer = document.querySelector('.menu_container')
 
 const toggling = () => {
     if (!sidebar.classList.contains('close')) {
         sidebar.classList.add('close')
     }
     sidebarClose.addEventListener('click', function () {
-        console.log("class avail");
         sidebar.classList.remove('close')
     })
+    menuContainer.addEventListener('mouseenter', function () {
+        sidebar.classList.remove('close')
+    })
+    menuContainer.addEventListener('mouseleave', function () {
+        sidebar.classList.add('close')
+    })
 }
-
+ 
 lockIcon.addEventListener('click', toggling)
